21de15b65d48f2994d7d251c78236d6018b0dde8
ota <otausuk@gmail.com>
2017-01-03 00:22:56 +0900

    Modify MeCab and travis

--- src/test/java/jp/assoon/MeCabTest.java
+++ src/test/java/jp/assoon/MeCabTest.java
@@ -19,6 +19,8 @@ public class MeCabTest {
 	static{
 		//テスト用のファイル作成
 		List<String> tempList = new ArrayList<>();
+		tempList.add("妊娠した時に仕事を９ヶ月ぐらいまでしていたので、なかなか赤ちゃんの必要なものが買えなかったのと、出産費用の事で色々買ってあげれなかったので出産費用をもう少し安くするかなにか必要な物をプレゼントしてほしい。");
+		tempList.add("近くに住むママ友がいません。近くに住むママと知りあえるきかいがほしいです。");
 		tempList.add("育休復帰は新しく仕事を始めるよりとっても楽でしたよ！");
 		tempList.add("残業多いけど子どものためにも頑張ります");
 		tempList.add("子育ても仕事もムリなく働ける場所をみつけたい！");
@@ -35,7 +37,9 @@ public class MeCabTest {
 		
 		//結果確認用のリスト
 		tempList = new ArrayList<>();
-		tempList.add("12");
+		tempList.add("14");
+		tempList.add("妊娠 仕事 赤ちゃん 出産 費用 出産 費用 プレゼント");
+		tempList.add("ママ 友 ママ");
 		tempList.add("育休 復帰 仕事");
 		tempList.add("残業 子ども");
 		tempList.add("子育て 仕事 場所");
